1130786f7be1bee428cdaa27663ac954c068109d
Lasklinikov <wuchenyuan@mail.ustc.edu.cn>
2026-02-06 19:39:13 +0800

    fix: handle zero remaining length case

--- FlexRAG/src/data.py
+++ FlexRAG/src/data.py
@@ -699,13 +699,14 @@ class Data:
                 # longbench    
                 len_for_encoder = lm_max_length - len(head_input_ids) - len(tail_input_ids) - len(normal_token_input_ids_pair[0]) - len(normal_token_input_ids_pair[1])
                 remain_length = len_for_encoder - low_ratio_index_length - len(selected_sentences_id) * 2 - len(encoder_input_ids)
-            if remain_length < 0:
-                raise ValueError("The remain must be greater than 0. Consider lowering the text_proportion.")
-
-            high_comp_ratio = math.ceil((len_encoder_input_ids - sentences_length) / remain_length)
+            
+            if remain_length <= 0:
+                high_comp_ratio = 0
+            else:
+                high_comp_ratio = math.ceil((len_encoder_input_ids - sentences_length) / remain_length)
 
             # * encoder_indices
-            if low_ratio_index_length > 0:
+            if low_ratio_index_length > 0 and high_comp_ratio > 0:
                 encoder_indices = Data.get_sentence_level_weighted_encoder_indices(encoder_input_ids, high_comp_ratio, low_comp_ratio, encoder_max_length, selected_sentences_id,  sentence_begin_indices, sentences_ids_list, down_scaling_method)
                 # make sure that lm_length is less than len_for_encoder
                 lm_length = sum([len(x) for x in encoder_indices])
